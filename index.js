9306919edda4a2d7273b7e8c49aab35a50fc5e1a
linux_china <libing.chen@gmail.com>
2021-09-30 01:57:58 -0700

    Disable log

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ const brotliCompress = async ({path}) => {
     const originalPath = path.replace("?br", "");
     const rawBytes = await Deno.readFile(originalPath);
     const compressedBytes = compress(rawBytes);
-    const uncompressedLength = rawBytes.length;
+    /*const uncompressedLength = rawBytes.length;
     const compressedLength = compressedBytes.length;
     let percent = Math.floor((uncompressedLength - compressedLength) / uncompressedLength * 100);
-    console.log(`brotli compress for ${originalPath}: ${percent}%  ${uncompressedLength} => ${compressedLength}`);
+    console.log(`brotli compress for ${originalPath}: ${percent}%  ${uncompressedLength} => ${compressedLength}`);*/
     return {contents: compressedBytes, loader: "binary"};
 }
 
